d2765de50e487058b59fc31ce4a80354948c5d77
LuffiDeMonk <thapaprabhat4@gmail.com>
2024-03-26 19:11:01 +0545

    error fixed

--- app/(protected)/dashboard/skills/page.tsx
+++ app/(protected)/dashboard/skills/page.tsx
@@ -1,31 +1,16 @@
 import FormContainer from '@/components/dashboard/FormContainer'
 import Heading from '@/components/dashboard/Heading'
 import SkillForm from '@/components/dashboard/skills/SkillForm'
-import { GetSkills } from '@/helpers/data/fetchSkills'
-import { ISkill } from '@/models/skill'
-import { Document } from 'mongoose'
 import React from 'react'
 
-type API = {
-    message: string,
-    skills: ISkill[]
-}
 
 export default async function Skills() {
-    const data: API = await GetSkills()
     return (
         <>
             <Heading title='Add Skills' />
             <FormContainer>
                 <SkillForm />
             </FormContainer>
-            <div>
-                {
-                    data.skills.map(skill => (
-                        <h1 key={skill._id.toString()}>{skill.title}</h1>
-                    ))
-                }
-            </div>
         </>
     )
 }
--- helpers/data/fetchSkills.ts
+++
@@ -1,9 +0,0 @@
-export const GetSkills = async <T>(): Promise<T> => {
-    const response = await fetch(`${process.env.HOMEPAGE}/api/skills`, {
-        next: {
-            revalidate: 3600,
-            tags: ['skill']
-        }
-    })
-    return response.json()
-}
